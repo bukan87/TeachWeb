59f905e6db8843133b32dc3a58ee8992a702931a
Bukan87 <bukan87@mail.ru>
2017-06-03 11:05:10 +0300

    Настройка кодогенрации на основе wsdl

--- Application/TeachWeb/recruiter-service/src/main/java/ru/bukan/TeachWeb/service/Publisher.java
+++ Application/TeachWeb/recruiter-service/src/main/java/ru/bukan/TeachWeb/service/Publisher.java
@@ -1,12 +1,10 @@
 package ru.bukan.TeachWeb.service;
 
-import javax.xml.ws.Endpoint;
-
 /**
  * @author by Ilin_ai on 31.05.2017.
  */
 public class Publisher {
     public static void main(String[] args){
-        Endpoint.publish("http://localhost:9999/VacancyService", new VacancyServiceImpl());
+        //Endpoint.publish("http://localhost:9999/VacancyService", new VacancyServiceImpl());
     }
 }
